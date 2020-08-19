e1de871e3dbae5de6874261c947a90cea94fb327
李无名 <1459543371@qq.com>
2020-08-19 15:46:28 +0800

    asa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: '爱书栈',
     description: '学习笔记',
     dest: './dist',
-	base:'/bookstack/dist/',
+	base:'/',
     port: 80,
     head: [
         ['link', {rel: 'icon', href: '/logo.jpg'}]
